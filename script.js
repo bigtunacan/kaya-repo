fc205ebbed7fbdcf7937abdf5dfc3dd9451de64c
Tim Fong <timfong888@gmail.com>
2014-10-08 21:25:18 -0700

    init

---
+++ script.js
@@ -0,0 +1,35 @@
+angular.module("MainModule", [])
+  .controller("MainController", function ($scope, $http)
+  {
+	$scope.chats = [];  
+    $scope.newChat = "type here";
+	$scope.errors = [];
+	
+    $scope.submitData = function ()
+    {
+	  //$scope.newChat = "submitting data..."	
+		
+	  /* scope attaches to View */	
+
+      /* $http.post("http://requestb.in/qwrtysqw", null, config)*/
+	  $scope.chats.push($scope.newChat) /*this gets moved to success */
+	  
+	  $http({
+		  method: "POST",
+		  url: "https://worker-aws-us-east-1.iron.io/2/projects/542c8609827e3f0005000123/tasks/webhook?code_name=botweb&oauth=LOo5Nc0x0e2GJ838_nbKoheXqM0",
+		  data: {input: $scope.newChat}
+	  })
+	    .success(function (data)
+        {
+         // $scope.chats.push(data);
+		  $scope.chats.push($scope.newChat)
+		  
+        })
+        .error(function (data)
+        {
+          $scope.errors.push(data);
+        });
+		
+    };
+		
+  });
---
+++ web_post.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/hello/:name' do
+  params[:name]
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+
